841d0f875fa85be95a349506bdec539f800e7d9b
Mohd Saad <developedbysaad@gmail.com>
2024-03-05 22:19:44 +0530

    Update initial-test-for-props.cy.js

--- l1/cypress/e2e/initial-test-for-props.cy.js
+++ l1/cypress/e2e/initial-test-for-props.cy.js
@@ -11,7 +11,7 @@ describe("When visiting the homepage,", () => {
   });
 
   it("the application should show `pending` and `done` lists of tasks ", () => {
-    cy.contains("pending", { matchCase: false });
-    cy.contains("done", { matchCase: false });
+    cy.contains('pending', { matchCase: false, timeout: 10000 });
+    cy.contains('done', { matchCase: false, timeout: 10000 });
   });
 });
